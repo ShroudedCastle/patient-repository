baa2b38ec1af0abd797007c4fdf07c44725a6a6b
Kevin Clark <KClark036@gmail.com>
2018-10-28 14:16:23 -0400

    project shell

---
+++ src/main/java/com/rwj/PatientRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.rwj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientRepositoryApplication.class, args);
+	}
+}
